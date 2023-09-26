4149bbedb370e2a38575c8b63d7c48d350f97a31
ten66 <ikesho.1217@gmail.com>
2023-09-26 21:30:36 +0900

    style: #1 homePageの簡易画面作成

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('ToDo App'),
+      ),
+      body: const Center(
+        child: Text('リスト一覧画面'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
