eb3cd2bb5606394c6303fdcddec368341e5164ca
ten66 <ikesho.1217@gmail.com>
2023-09-26 23:40:48 +0900

    style: #1 タスク追加用のダイアログを実装

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_todo_app/constants.dart';
 
@@ -21,7 +22,42 @@ class HomePage extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: kAppBarColor,
-        onPressed: () {},
+        onPressed: () => showCupertinoDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) => CupertinoAlertDialog(
+            title: const Text('タスク追加'),
+            // content: const TextField(
+            //   obscureText: true,
+            //   decoration: InputDecoration(
+            //     border: OutlineInputBorder(),
+            //     labelText: 'Password',
+            //   ),
+            // ),
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'Cancel'),
+                child: const Text(
+                  'Cancel',
+                  style: TextStyle(
+                    color: kAppBarColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'Add'),
+                child: const Text(
+                  'Add',
+                  style: TextStyle(
+                    color: kAppBarColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
         tooltip: 'increment',
         child: const Icon(Icons.add),
       ),
