d7d514171badbda3de9cb8c782c3c79fedd962a4
ten66 <ikesho.1217@gmail.com>
2023-09-26 21:43:08 +0900

    style: #1 appBarのtitleの削除:再

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,7 +8,6 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('TODO App'),
       ),
       body: const Center(
         child: Text('リスト一覧画面'),
