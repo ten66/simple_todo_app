b1724d42a59438541df3d10c1528e8d883f68fa4
ten66 <ikesho.1217@gmail.com>
2023-09-28 07:27:13 +0900

    style: #1 タスク追加ボタンをwidget化

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_todo_app/constants.dart';
 import 'package:simple_todo_app/widgets/task_card.dart';
+
+import '../widgets/add_task_button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -56,44 +57,7 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: kAppBarColor,
-        onPressed: () => showCupertinoDialog(
-          context: context,
-          barrierDismissible: false,
-          builder: (BuildContext context) => CupertinoAlertDialog(
-            title: const Text('タスク追加'),
-            content: const CupertinoTextField(
-              placeholder: 'task name',
-              cursorColor: kAppBarColor,
-            ),
-            actions: [
-              TextButton(
-                onPressed: () => Navigator.pop(context, 'Cancel'),
-                child: const Text(
-                  'Cancel',
-                  style: TextStyle(
-                    color: kAppBarColor,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              TextButton(
-                onPressed: () => Navigator.pop(context, 'Add'),
-                child: const Text(
-                  'Add',
-                  style: TextStyle(
-                    color: kAppBarColor,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        tooltip: 'increment',
-        child: const Icon(Icons.add),
-      ),
+      floatingActionButton: const AddTaskButton(),
     );
   }
 }
