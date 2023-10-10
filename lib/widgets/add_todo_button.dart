87edc0b42489889e971a136a35768b94e049e490
ten66 <ikesho.1217@gmail.com>
2023-10-10 23:37:35 +0900

    #9 addTodoButton内の状態管理をRiverpodに変更

--- lib/widgets/add_todo_button.dart
+++ lib/widgets/add_todo_button.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:simple_todo_app/todo_list.dart';
+import 'package:simple_todo_app/widgets/todo.dart';
+import 'package:uuid/uuid.dart';
 
 import '../constants.dart';
 
-class AddTodoButton extends StatelessWidget {
-  final dynamic controller;
-  final VoidCallback onAdd;
+class AddTodoButton extends ConsumerWidget {
+  // final dynamic controller;
+  // final VoidCallback onAdd;
   final VoidCallback onCancel;
 
   const AddTodoButton({
     super.key,
-    required this.controller,
-    required this.onAdd,
+    // required this.controller,
+    // required this.onAdd,
     required this.onCancel,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    var uuid = const Uuid();
+    final controller = TextEditingController();
+
     return FloatingActionButton(
       backgroundColor: kAppBarColor,
       onPressed: () => showCupertinoDialog(
@@ -44,7 +51,13 @@ class AddTodoButton extends StatelessWidget {
               ),
             ),
             TextButton(
-              onPressed: onAdd,
+              // onPressed: onAdd,
+              onPressed: () {
+                final notifier = ref.read(todoListNotifierProvider.notifier);
+                String id = uuid.v4();
+                Todo newTodo = Todo(id: id, title: controller.text);
+                notifier.addTodo(newTodo);
+              },
               child: const Text(
                 '追加',
                 style: TextStyle(
