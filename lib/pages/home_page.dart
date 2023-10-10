28dd8fc15f82fbf8b3ba8e1bed3d2fb0d55c29e1
ten66 <ikesho.1217@gmail.com>
2023-10-11 00:32:09 +0900

    #9 todoListView Widgetを作成し、HomePageファイルの可読性を向上

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,54 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:simple_todo_app/constants.dart';
 import 'package:simple_todo_app/pages/setting_page.dart';
-import 'package:simple_todo_app/todo_list.dart';
 import 'package:simple_todo_app/widgets/delete_todo_button.dart';
-import 'package:simple_todo_app/widgets/todo.dart';
-import 'package:simple_todo_app/widgets/todo_card.dart';
 import 'package:simple_todo_app/widgets/add_todo_button.dart';
-import 'package:uuid/uuid.dart';
+import 'package:simple_todo_app/widgets/todo_list_view.dart';
 
-class HomePage extends ConsumerWidget {
-  HomePage({super.key});
-
-  final _controller = TextEditingController();
-
-  // List<Todo> todoList = [];
-  var uuid = const Uuid();
-
-  // void checkBoxChanged(bool? value, int index) {
-  //   setState(() {
-  //     todoList[index] = !todoList[index].isCompleted
-  //         ? todoList[index].copyWith(isCompleted: true)
-  //         : todoList[index].copyWith(isCompleted: false);
-  //   });
-  // }
-
-  // void addNewTodo() {
-  //   if (_controller.text != '') {
-  //     setState(() {
-  //       final id = uuid.v4();
-  //       Todo newTodo = Todo(id: id, title: _controller.text);
-  //       todoList.add(newTodo);
-  //       _controller.clear();
-  //     });
-  //   }
-
-  //   Navigator.pop(context);
-  // }
-
-  // void deleteTodo() {
-  //   setState(() {
-  //     todoList = todoList.where((todo) => !todo.isCompleted).toList();
-  //   });
-  //   Navigator.pop(context);
-  // }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final todoList = ref.watch(todoListNotifierProvider);
-
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBackgroundColor,
       appBar: AppBar(
@@ -63,52 +24,13 @@ class HomePage extends ConsumerWidget {
             );
           },
         ),
-        actions: [
-          DeleteTodoButton(
-            // onDelete: deleteTodo,
-            onCancel: () => Navigator.pop(context),
-          ),
-          const SizedBox(width: 15),
+        actions: const [
+          DeleteTodoButton(),
+          SizedBox(width: 15),
         ],
       ),
-      body: Visibility(
-        visible: todoList.isEmpty,
-        replacement: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: ReorderableListView.builder(
-            padding: const EdgeInsets.only(top: 20, bottom: 40),
-            proxyDecorator: (child, index, animation) => Material(
-              type: MaterialType.transparency,
-              child: child,
-            ),
-            shrinkWrap: true,
-            physics: const ClampingScrollPhysics(),
-            itemCount: todoList.length,
-            itemBuilder: (context, index) => Container(
-              key: ValueKey(todoList[index]),
-              child: TodoCard(
-                todo: todoList[index],
-                onChanged: (value) => checkBoxChanged(value, index),
-              ),
-            ),
-            onReorder: (oldIndex, newIndex) {
-              setState(() {
-                if (oldIndex < newIndex) newIndex--;
-                final Todo todo = todoList.removeAt(oldIndex);
-                todoList.insert(newIndex, todo);
-              });
-            },
-          ),
-        ),
-        child: const Center(
-          child: Text('TODOがありません'),
-        ),
-      ),
-      floatingActionButton: AddTodoButton(
-        controller: _controller,
-        onAdd: addNewTodo,
-        onCancel: () => Navigator.pop(context),
-      ),
+      body: const TodoListView(),
+      floatingActionButton: const AddTodoButton(),
     );
   }
 }
