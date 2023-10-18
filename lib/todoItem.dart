84f0370c62af99f32840ef4ce73f39fffde5bcf8
ten66 <ikesho.1217@gmail.com>
2023-10-18 23:35:19 +0900

    #17 hiveObjectの作成

---
+++ lib/todoItem.dart
@@ -0,0 +1,15 @@
+import 'package:hive/hive.dart';
+
+part 'todo_item.g.dart';
+
+@HiveType(typeId: 0)
+class TodoItem extends HiveObject {
+  @HiveField(0)
+  String id;
+
+  @HiveField(1)
+  String title;
+
+  @HiveField(2)
+  bool isCompleted = false;
+}
